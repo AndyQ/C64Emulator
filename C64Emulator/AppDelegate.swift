28df929d4e5a541a71a60a82d943324d2220e9a2
AndyQ <andy.qua@gmail.com>
2017-01-08 13:16:39 +0000

    Removed test code!

--- C64Emulator/AppDelegate.swift
+++ C64Emulator/AppDelegate.swift
@@ -49,7 +49,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         
-        removeAllGames()
         return true
     }
 
